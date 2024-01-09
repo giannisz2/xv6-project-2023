9e1f3f4f71504652add6132eb0f591501038e052
giannisz2 <giannisz2001@gmail.com>
2024-01-09 23:24:13 +0200

    Created pstat.h for the new struct

---
+++ xv6-project-2023/kernel/pstat.h
@@ -0,0 +1,11 @@
+#include "param.h"
+
+// Definition of pstat struct
+struct pstat {
+  int pid[NPROC]; // Process id
+  int ppid[NPROC]; // Parent process id
+  char* name[NPROC][20]; // Name of process assuming 20 is the max length of characters for a process
+  int priority[NPROC]; // Priority of process
+  int state[NPROC]; // Current status of process
+  int length[NPROC]; // Length of the process
+};
