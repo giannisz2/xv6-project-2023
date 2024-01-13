0559662b84d848881ab39e6a747f30dc64561a1c
giannisz2 <giannisz2001@gmail.com>
2024-01-13 12:34:39 +0200

    Fixed ps.c

--- xv6-project-2023/user/ps.c
+++ xv6-project-2023/user/ps.c
@@ -11,12 +11,11 @@ int main(void) {
         printf("Failure in ps file.\n");
         return -1;
     }
-    printf("Name\tPID\t\tPPID\tPriority\tState\tLength");
-    for(int i = 0; i < NPROC; i++) {
-        if(pstat->pid[i] == 0) {
-            continue;
-        }
-        printf("%s\t%d\t%d\t%d\t%d\t%d", pstat->name[i], pstat->pid[i], pstat->ppid[i], pstat->priority[i], pstat->state[i], pstat->length[i]);
+
+    if(printpinfo(pstat) < 0) {
+        printf("Failure in ps file.\n");
+        return -1;
     }
+    
     return 0;
 }
