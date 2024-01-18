e0b4781872119c29c89a16e12fd578fcfd0219b6
giannisz2 <giannisz2001@gmail.com>
2024-01-18 15:00:21 +0200

    Fixed ps.c

--- xv6-project-2023/user/ps.c
+++ xv6-project-2023/user/ps.c
@@ -1,15 +1,28 @@
-#include "user.h"
 #include "kernel/types.h"
-#include "kernel/pstat.h"
 #include "kernel/stat.h"
-
+#include "user.h"
+#include "kernel/spinlock.h"
+#include "kernel/param.h"
+#include "kernel/riscv.h"
+#include "kernel/pstat.h"
+#include "kernel/proc.h"
 
 int main(void) {
-    struct pstat* pstat = malloc(sizeof(struct pstat));
-    
-    if(getpinfo(pstat) < 0) {
-        printf("Failure in ps file.\n");
-        return -1;
+
+  struct pstat* pstat = malloc(sizeof(struct pstat));  
+  
+  if(getpinfo(pstat) < 0) {
+    printf("Error in ps.c\n");
+    return -1;
+  }
+
+  printf("Name\tPID\tPPID\tPriority  State\tLength\n");
+
+  for (int i = 0; i < NPROC; i++) {
+    if (pstat->state[i] != UNUSED && pstat->pid[i] != 0) { 
+      printf("%s\t%d\t%d\t%d\t  %d\t%d\n", pstat->name[i], pstat->pid[i], pstat->ppid[i], pstat->priority[i], pstat->state[i], pstat->length[i]);
     }
-    return 0;
+  }
+
+  return 0;
 }
